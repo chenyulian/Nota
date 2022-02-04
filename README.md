f9c7eb95e82abc194ba975df05b1aaf2526b4d13
chenyulian <648270560@qq.com>
2022-02-04 21:58:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# About Nota
+Nota is a block-based note-taking app.
+
+Nota is based on Vue 3 + Typescript + Vite.
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Welcome to Nota!
+    </div>
+</template>
+<script lang="ts">
+export default {
+    
+}
+</script>
