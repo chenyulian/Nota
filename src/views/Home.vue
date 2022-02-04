54a54cf75626bc32b87e8fea87586cd2e9e0455b
chenyulian <648270560@qq.com>
2022-02-04 22:30:08 +0800

    引入editorjs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        Welcome to Nota!
+        <p style="text-align: center">Welcome to Nota!</p>
+        <div id="editorjs">
+
+        </div>
     </div>
 </template>
 <script lang="ts">
+import EditorJS from '@editorjs/editorjs'; 
+import { onMounted } from '@vue/runtime-core';
+
 export default {
-    
+    setup() {
+        onMounted(()=>{
+            (Window as any).editor = new EditorJS('editorjs')
+        })
+    }
 }
 </script>
